40643334e338b4260d39d9278b48bb0e353abac5
Maiko Stievem <msdraco@gmail.com>
2020-05-31 11:26:00 -0300

    project first setup

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Users from '../screens/users';
+import Custumers from '../screens/customers';
+import CustumerDetails from '../screens/customerDetails';
+
+const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Users} />
+      <Route path="/customers" exact component={Custumers} />
+      <Route path="/customers/details" component={CustumerDetails} />
+    </Switch>
+  );
+};
+
+export default Routes;
---
+++ src/screens/customerDetails/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CustomerDetails: React.FC = () => {
+  return <h1>CustomerDetails</h1>;
+};
+
+export default CustomerDetails;
